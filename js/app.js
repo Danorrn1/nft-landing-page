cbd9fd540e83efab2fcdc1ce83a15477af9b1e56
Danorrn1 <feelthroughmyeyes@hotmail.com>
2023-01-26 20:24:01 -0500

    Updated Colors, pics and Text

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 // METAMASK CONNECTION
 const TIMEOUT = 1000;
-const COLLECTION_NAME = 'CodeCats';
+const COLLECTION_NAME = 'Dungeons and Dragons Pixel Gif Collectibles';
 let editions = [];
 let dots = 1;
 
